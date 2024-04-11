dadcad48c9cb1b5ca6253edb128d64980e0d802a
T. Hamilton <profhamilton3@gmail.com>
2024-04-11 11:21:26 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,15 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (0 == receivedNumber) {
-        wuKong.stopAllMotor()
         wuKong.mecanumStop()
         basic.showIcon(IconNames.Sad)
     } else if (1 == receivedNumber) {
-        wuKong.setAllMotor(100, 100)
         wuKong.mecanumRun(wuKong.RunList.Front, 100)
         basic.showIcon(IconNames.Asleep)
     } else if (2 == receivedNumber) {
-        wuKong.setAllMotor(-50, 75)
         wuKong.mecanumSpin(wuKong.TurnList.Left, 75)
     } else if (3 == receivedNumber) {
-        wuKong.setAllMotor(75, -50)
         wuKong.mecanumSpin(wuKong.TurnList.Right, 75)
     } else if (4 == receivedNumber) {
-        wuKong.setAllMotor(-75, -75)
         wuKong.mecanumRun(wuKong.RunList.rear, 100)
     } else {
         basic.showIcon(IconNames.Skull)
